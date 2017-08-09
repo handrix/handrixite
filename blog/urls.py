1ad58ad3325343c2e8ebd7074a72898054aa9252
handrix <zequnfeng@163.com>
2017-08-09 15:54:18 +0800

    repo迁移

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+from django.conf.urls import url
+from .views import IndexView, BlogDetailView, CategoryView, TagView, AboutMeView
+
+urlpatterns = [
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^about/$', AboutMeView.as_view(), name='about'),
+    url(r'^article/(?P<article_id>\d+)$', BlogDetailView.as_view(), name='detail'),
+    url(r'^category/(?P<cate_id>\d+)$', CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<tag_id>\d+)$', TagView.as_view(), name='tag'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,82 @@
-from django.shortcuts import render
+# -*- coding:utf-8 -*-
 
-# Create your views here.
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
+from . import models
+import markdown2
+
+
+class IndexView(ListView):
+    template_name = "blog/index.html"
+    context_object_name = "article_list"
+
+    def get_queryset(self):
+        article_list = models.Article.objects.all()
+        return article_list
+        pass
+
+    def get_context_data(self, **kwargs):
+        kwargs['category_list'] = models.Category.objects.all().order_by('name')
+        kwargs['tag_list'] = models.Tag.objects.all().order_by('name')
+        return super(IndexView, self).get_context_data(**kwargs)
+        pass
+    pass
+
+
+class BlogDetailView(DetailView):
+    template_name = "blog/detail.html"
+    context_object_name = "article"
+    pk_url_kwarg = 'article_id'
+
+    def get_queryset(self):
+        article_list = models.Article.objects.all()
+        return article_list
+        pass
+
+    def get_object(self, queryset=None):
+        obj = super(BlogDetailView, self).get_object()
+        obj.body = markdown2.markdown(obj.body, extras=['fenced-code-blocks'], )
+        return obj
+        pass
+    pass
+
+
+class CategoryView(ListView):
+    template_name = "blog/index.html"
+    context_object_name = "article_list"
+
+    def get_queryset(self):
+        article_list = models.Article.objects.filter(category=self.kwargs['cate_id'], status='p')
+        for article in article_list:
+            article.body = article.body
+        return article_list
+        pass
+
+    def get_context_data(self, **kwargs):
+        kwargs['category_list'] = models.Category.objects.all().order_by('name')
+        return super(CategoryView, self).get_context_data(**kwargs)
+        pass
+    pass
+
+
+class TagView(ListView):
+    template_name = "blog/index.html"
+    context_object_name = "article_list"
+
+    def get_queryset(self):
+        article_list = models.Article.objects.filter(tags=self.kwargs['tag_id'], status='p')
+        for article in article_list:
+            article.body = article.body
+        return article_list
+        pass
+
+    def get_context_data(self, **kwargs):
+        kwargs['tag_list'] = models.Tag.objects.all().order_by('name')
+        return super(TagView, self).get_context_data(**kwargs)
+        pass
+    pass
+
+
+class AboutMeView(TemplateView):
+    template_name = "blog/about.html"
